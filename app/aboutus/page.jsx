156ef695601b2c12fafd032a568f04c820c87e06
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-06-21 12:28:35 +0100

    about us page and logo added

---
+++ app/aboutus/page.jsx
@@ -0,0 +1,331 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <section className="bg-fifth text-primary">
+      <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+        <div className="max-w-xl">
+          <h2 className="text-3xl font-bold sm:text-4xl">About Us</h2>
+
+          <p className="mt-4 text-gray-300">
+            Global Realty Management specialises in the buying, selling, and
+            management of real estate in both the UK and Turkey. We provide
+            reliable, professional, and personalised services to ensure our
+            clients get the best out of their property investments. With years
+            of experience and extensive industry knowledge, our team delivers
+            successful and sustainable solutions in the real estate sector.
+          </p>
+        </div>
+
+        <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                ></path>
+              </svg>
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Our Mission</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Our mission is to make property investments easier for our
+                clients by offering the highest quality and value, while acting
+                as a trustworthy guide throughout the process. We prioritise
+                transparency and integrity at every step, ensuring a smooth
+                experience for both buyers and sellers.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                ></path>
+              </svg>
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Our Vision</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Our vision is to be recognised as a leading brand in the global
+                real estate sector, setting ourselves apart with innovative
+                services. By combining technology and expertise, we aim to
+                provide our clients with the most modern and practical
+                solutions, not just in the UK and Turkey but on an international
+                scale. We strive to be a reliable partner in the global real
+                estate market by prioritising customer satisfaction.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+        <div className="max-w-xl">
+          <h2 className="text-3xl font-bold sm:text-4xl">Our Services</h2>
+        </div>
+
+        <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                ></path>
+              </svg>
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">
+                Real Estate Buying and Selling Consultancy
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Our experienced team offers comprehensive consultancy services
+                for the buying and selling processes of real estate. We assist
+                with market analysis, pricing strategies, and negotiation
+                processes.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                ></path>
+              </svg>
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Property Management</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                We ensure your properties are professionally managed, including
+                rental, maintenance, repairs, rent collection, and strategies to
+                increase property value.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                ></path>
+              </svg>
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Investment Consultancy</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                We develop and implement personalised investment strategies to
+                ensure the best returns on your real estate investments. We
+                provide all the support needed to identify and evaluate the
+                right investment opportunities.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+        <div className="max-w-xl">
+          <h2 className="text-3xl font-bold sm:text-4xl">Why Choose Us</h2>
+        </div>
+
+        <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                ></path>
+              </svg>
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Experience and Expertise</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                OWith our extensive experience and expert team, we guarantee the
+                best service at all times.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                ></path>
+              </svg>
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">• Personalised Service</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                We listen carefully to each client’s needs and goals, offering
+                tailored solutions.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                ></path>
+              </svg>
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Trust and Transparency</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                We build trustworthy relationships with our clients by adhering
+                to principles of transparency and honesty at every step of our
+                business processes.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+        <div className="max-w-xl">
+          <h2 className="text-2xl font-bold sm:text-3xl">Contact Us</h2>
+        </div>
+
+        <div className="mt-8  gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                ></path>
+              </svg>
+            </span>
+
+            <div>
+              <p className="mt-1 text-large text-gray-300">
+                To reach us and learn more about our services, you can fill out
+                our contact form. Global Realty Management is always by your
+                side for all your real estate needs!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default page;

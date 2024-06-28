f88a442327a395cdcf44c7c35b61eb120a5324b6
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-06-28 22:57:52 +0100

    mail, phone and contact hrefsadded

--- app/_components/MainLead.jsx
+++ app/_components/MainLead.jsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import React from "react";
+import { FaWpforms } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
+import { FaAt } from "react-icons/fa";
 
 const MainLead = () => {
   return (
@@ -27,20 +30,10 @@ const MainLead = () => {
           <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
             <Link
               className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-              href="#"
+              href="tel:+447438214612"
             >
               <span className="inline-block rounded-lg bg-primary p-3">
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  class="icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                  width="3em"
-                  height="3em"
-                >
-                  <path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z" />
-                </svg>
+                <FaPhone size={45} />
               </span>
 
               <h2 className="mt-2 font-bold text-xl">Call Us</h2>
@@ -52,20 +45,10 @@ const MainLead = () => {
 
             <Link
               className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-              href="#"
+              href="mailto:metincelik75@gmail.com"
             >
               <span className="inline-block rounded-lg bg-primary p-3">
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  class="icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                  width="3em"
-                  height="3em"
-                >
-                  <path d="M256 64C150 64 64 150 64 256s86 192 192 192c17.7 0 32 14.3 32 32s-14.3 32-32 32C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256v32c0 53-43 96-96 96c-29.3 0-55.6-13.2-73.2-33.9C320 371.1 289.5 384 256 384c-70.7 0-128-57.3-128-128s57.3-128 128-128c27.9 0 53.7 8.9 74.7 24.1c5.7-5 13.1-8.1 21.3-8.1c17.7 0 32 14.3 32 32v80 32c0 17.7 14.3 32 32 32s32-14.3 32-32V256c0-106-86-192-192-192zm64 192a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z" />
-                </svg>
+                <FaAt size={45} />
               </span>
 
               <h2 className="mt-2 font-bold text-xl">Email Us</h2>
@@ -77,21 +60,10 @@ const MainLead = () => {
 
             <Link
               className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-              href="#"
+              href="/contact"
             >
               <span className="inline-block rounded-lg bg-primary p-3">
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  class="icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                  width="3em"
-                  stroke="currentColor"
-                  height="3em"
-                >
-                  <path d="M448 75.2v361.7c0 24.3-19 43.2-43.2 43.2H43.2C19.3 480 0 461.4 0 436.8V75.2C0 51.1 18.8 32 43.2 32h361.7c24 0 43.1 18.8 43.1 43.2zm-37.3 361.6V75.2c0-3-2.6-5.8-5.8-5.8h-9.3L285.3 144 224 94.1 162.8 144 52.5 69.3h-9.3c-3.2 0-5.8 2.8-5.8 5.8v361.7c0 3 2.6 5.8 5.8 5.8h361.7c3.2 .1 5.8-2.7 5.8-5.8zM150.2 186v37H76.7v-37h73.5zm0 74.4v37.3H76.7v-37.3h73.5zm11.1-147.3l54-43.7H96.8l64.5 43.7zm210 72.9v37h-196v-37h196zm0 74.4v37.3h-196v-37.3h196zm-84.6-147.3l64.5-43.7H232.8l53.9 43.7zM371.3 335v37.3h-99.4V335h99.4z" />
-                </svg>
+                <FaWpforms size={45} />
               </span>
 
               <h2 className="mt-2 font-bold text-xl">Fill the Form</h2>

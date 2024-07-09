ef2f9ac9e96374daeb3a2ba01629c15b0dc22ecc
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-07-09 14:02:47 +0100

    slider component is changed to multiple usage

--- app/_atoms/Slide.jsx
+++ app/_atoms/Slide.jsx
@@ -1,53 +1,53 @@
 import Link from "next/link";
 import React from "react";
-import { getSlide } from "../_utils/GlobalApi";
 
-const Slide = async () => {
-  const data = await getSlide();
+const Slide = ({ slide }) => {
+  const {
+    sliderHeader,
+    sliderDescription,
+    sliderButtonFirst,
+    sliderButtonSecond,
+    sliderImage,
+  } = slide.attributes;
+
   return (
-    <div>
-      {data.map((item, index) => {
-        return (
-          <section
-            key={index}
-            style={{
-              backgroundImage: `url(https://panel.theglobalproperty.co.uk${item?.attributes?.sliderImage?.data?.attributes?.url})`,
-            }}
-            className={`relative bg-cover bg-center bg-no-repeat`}
-          >
-            <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 "></div>
+    <section
+      key={slide.id}
+      className="relative bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: `url(https://panel.theglobalproperty.co.uk${sliderImage.data.attributes.url})`,
+      }}
+    >
+      <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 "></div>
 
-            <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
-              <div className="max-w-xl text-left ltr:sm:text-left rtl:sm:text-right">
-                <h1 className="text-3xl font-extrabold sm:text-5xl text-gray-900 sm:text-white">
-                  {item?.attributes?.sliderHeader}
-                </h1>
+      <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+        <div className="max-w-xl text-left ltr:sm:text-left rtl:sm:text-right">
+          <h1 className="text-3xl font-extrabold sm:text-5xl text-gray-900 sm:text-white">
+            {sliderHeader}
+          </h1>
 
-                <p className="mt-4 max-w-lg sm:text-xl/relaxed text-gray-900 sm:text-white">
-                  {item?.attributes?.sliderDescription}
-                </p>
+          <p className="mt-4 max-w-lg sm:text-xl/relaxed text-gray-900 sm:text-white">
+            {sliderDescription}
+          </p>
 
-                <div className="mt-8 flex flex-wrap gap-4 text-center">
-                  <Link
-                    href="#"
-                    className="block w-full rounded bg-fifth px-12 py-3 text-sm font-medium text-primary shadow hover:bg-primary hover:text-fifth ocus:outline-none focus:ring active:bg-third active:text-fifth sm:w-auto"
-                  >
-                    {item?.attributes?.sliderButtonFirst}
-                  </Link>
+          <div className="mt-8 flex flex-wrap gap-4 text-center">
+            <Link
+              href="#"
+              className="block w-full rounded bg-fifth px-12 py-3 text-sm font-medium text-primary shadow hover:bg-primary hover:text-fifth focus:outline-none focus:ring active:bg-third active:text-fifth sm:w-auto"
+            >
+              {sliderButtonFirst}
+            </Link>
 
-                  <Link
-                    href="#"
-                    className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-fifth shadow hover:bg-fifth hover:text-primary ocus:outline-none focus:ring active:bg-third active:text-fifth sm:w-auto"
-                  >
-                    {item?.attributes?.sliderButtonSecond}
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </section>
-        );
-      })}
-    </div>
+            <Link
+              href="#"
+              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-fifth shadow hover:bg-fifth hover:text-primary focus:outline-none focus:ring active:bg-third active:text-fifth sm:w-auto"
+            >
+              {sliderButtonSecond}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- app/_components/Slider.jsx
+++ app/_components/Slider.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Slide from "../_atoms/Slide";
+import { getSlide } from "../_utils/GlobalApi";
 
-const Slider = () => {
+const Slider = async () => {
+  const slides = await getSlide();
   return (
     <div>
-      <Slide />
+      {slides.map((slide, index) => (
+        <Slide key={slide.id} slide={slide} />
+      ))}
     </div>
   );
 };

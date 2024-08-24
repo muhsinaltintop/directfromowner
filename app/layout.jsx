16d74654904037c03e3d20fa80fedc2b69768b3b
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-24 15:45:14 +0100

    icon added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
-import SideMenu from "./_components/SideMenu";
 import Footer from "./_components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head><link rel="icon" sizes="32x32" href="/icon.png"/></head>
       <body className={inter.className}>
         <Header />
         <div className="flex">

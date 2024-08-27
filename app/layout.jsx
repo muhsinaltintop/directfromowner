9052c7367c7284c4c2b0b1bc9ea6d02ee0dd4c67
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-27 12:42:01 +0100

    meta tags fixed

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,18 +3,24 @@ import "./globals.css";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 import  Head  from "next/head";
+import GPLogo from "../public/favicon.ico"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Global Property",
   description: "UK Property Investment Experts",
+  image: "/icon.png",
+  url: "https://theglobalproperty.co.uk/",
+  type: "website"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head><link rel="icon" sizes="32x32" href="/favicon.ico"/></Head>
+      <Head>
+        <link rel="icon" sizes="32x32" href="/favicon.ico"/>
+        </Head>
       <body className={inter.className}>
         <Header />
         <div className="flex">

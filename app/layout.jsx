b3ccedf0f1c5a8a94e556ec09d603f23517095db
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-27 12:51:43 +0100

    meta tags changed again

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,22 +3,24 @@ import "./globals.css";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 import  Head  from "next/head";
+import GPLogo from "../public/favicon.ico"
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "Global Property: UK Property Investment Experts",
+  description: "We provide high-performance property investments in the UK, offering end-to-end support, from financial analysis to portfolio building, tailored to client needs.",
+  image: "https://panel.theglobalproperty.co.uk/uploads/icon_9ba4bcda9c.png",
+  url: "https://theglobalproperty.co.uk/",
+  type: "website"
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
         <link rel="icon" sizes="32x32" href="/favicon.ico"/>
-        <meta property="og:title" content="Global Property, UK Property Investment Experts" />
-        <meta property="og:description" content="We provide high-performance property investments in the UK, offering end-to-end support, from financial analysis to portfolio building, tailored to client needs." />
-        <meta property="og:image" content="https://panel.theglobalproperty.co.uk/uploads/icon_9ba4bcda9c.png" />
-        <meta property="og:url" content="https://theglobalproperty.co.uk/" />
-        <meta property="og:type" content="website" />
-
-        </Head>
+      </Head>
       <body className={inter.className}>
         <Header />
         <div className="flex">

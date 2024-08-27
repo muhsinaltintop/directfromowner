160dccf1be25915afe49c8875e01bc0b139f4e93
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-27 13:31:23 +0100

    Head removed and sharp installed

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 import  Head  from "next/head";
-import GPLogo from "../public/favicon.ico"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,15 +30,16 @@ export const metadata = {
     description: "We provide high-performance property investments in the UK, offering end-to-end support, from financial analysis to portfolio building, tailored to client needs.",
     images: ["https://panel.theglobalproperty.co.uk/uploads/icon_579502c12f.png"],
   },
+  icons: {
+    icon: "/favicon.ico", // Path to your favicon
+    sizes: "32x32",
+  },
 };
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" sizes="32x32" href="/favicon.ico"/>
-      </Head>
       <body className={inter.className}>
         <Header />
         <div className="flex">

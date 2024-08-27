6499a2115a7c42280a6ab0bbd0913d2fc1265116
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-27 13:00:32 +0100

    metatags for twitter added

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,10 +10,28 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Global Property: UK Property Investment Experts",
   description: "We provide high-performance property investments in the UK, offering end-to-end support, from financial analysis to portfolio building, tailored to client needs.",
-  image: "https://panel.theglobalproperty.co.uk/uploads/icon_9ba4bcda9c.png",
-  url: "https://theglobalproperty.co.uk/",
-  type: "website"
+  openGraph: {
+    title: "Global Property: UK Property Investment Experts",
+    description: "We provide high-performance property investments in the UK, offering end-to-end support, from financial analysis to portfolio building, tailored to client needs.",
+    url: "https://theglobalproperty.co.uk/",
+    images: [
+      {
+        url: "https://panel.theglobalproperty.co.uk/uploads/icon_9ba4bcda9c.png",
+        width: 800,
+        height: 600,
+        alt: "Global Property Logo",
+      }
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Global Property: UK Property Investment Experts",
+    description: "We provide high-performance property investments in the UK, offering end-to-end support, from financial analysis to portfolio building, tailored to client needs.",
+    images: ["https://panel.theglobalproperty.co.uk/uploads/icon_9ba4bcda9c.png"],
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
